a490e396c4ad6e29d180aa2240fc1823f904eace
Song Jin <song.jin@salesforce.com>
2021-06-17 18:02:09 -0700

    tweak

--- app/src/main/java/com/song/webview/SecondActivity.kt
+++ app/src/main/java/com/song/webview/SecondActivity.kt
@@ -1,8 +1,12 @@
 package com.song.webview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +15,21 @@ class SecondActivity : AppCompatActivity() {
 
         val url = intent.getStringExtra(Misc.URL)
 
+        val textView = findViewById<TextView>(R.id.second_textView)
+        textView.movementMethod = ScrollingMovementMethod()
+
         val webView = findViewById<WebView>(R.id.second_webView)
+        webView.settings.setJavaScriptEnabled(true)
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                webView.evaluateJavascript(
+                    "(function() { return ('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>'); })();"
+                ) { html ->
+                    textView.text = html
+                }
+            }
+        }
+
         if (url != null) {
             webView.loadUrl(url)
         }
